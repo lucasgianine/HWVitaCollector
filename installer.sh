6323b720e8c072a1405e0f07941d580ea85dec95
lucasgianine <lucas.esilva@sptech.school>
2023-10-17 20:34:21 -0300

    fix: Alterando caminho do curl e retirando o 'sudo apt upgrade'

--- installer.sh
+++ installer.sh
@@ -8,74 +8,81 @@ echo "==========================================================="
 echo "Você deseja instalar nosso software? [Y/n]"
 read getRes
 
-if [ "$getRes" = "Y" ]
+if [ "$getRes" = "Y" ] || [ "$getRes" = "y" ]
 	then	
-	# Verificando se o Java 17.x está instalado
-	java -version
+	java -version # Verificando a versão do Java
 
-	if [ $? -eq 0 ];
-		then
-		# Guardando a versão atual do usuário em uma variável
-		versao=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+	if [ $? -eq 0 ]
+        then
+        versao_atual=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+        versao_necessaria="17.0.6"
 
-		if [[ "$versao" == "17.0.6" ]]
-			then
-			echo "Java $versao já está instalado."
+        if [[ "$versao_atual" == "$versao_necessaria" ]]
+            then
+            echo "Java $versao_atual já está instalado."
 			echo "Prosseguindo com a instalação do software..."
-		else
-			echo "Java $versao está instalado, mas é inferior a versão necessária."
-			echo "Você deseja instalar a versão atual? [Y/n]"
-			read res
+        else 
+			echo "Java está instalado, mas é inferior a versão necessária."
+		    echo "Você deseja instalar a versão atual? [Y/n]"
+		    read res
 
-			if [ "$res" = "Y" ]
+		    if [ "$res" = "Y" ] || [ "$res" = "y" ]
 				then
-				echo "Inicializando a instalação..."
-				# Atualizando os pacotes do sistema
-				sudo apt update
-				# Atualizando a versão do Java para a versão necessário
-				sudo apt install openjdk-17-jdk -y
-                nova_versao=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+			    echo "Inicializando a instalação..." # Instalando o Java na versão 17.x
+			    sudo apt-get update # Atualizando os pacotes do sistema
+    			sudo apt install openjdk-17-jdk -y
+                nova_versao=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}') # Pegando a nova versão do Java instalado
 				echo "Java $nova_versao instalado com sucesso!"
-			else
-				echo "O Java não será instalado."
-				echo "Finalizando execução..."
+		    else
+			    echo "O Java não será instalado."
+    			echo "Finalizando execução..."
+                sleep 2 # Timeout de espera antes de finalizar o processo
 				exit 1 # Finalizando o processo e printando status de erro para o usuário
-			fi
-		fi
+		    fi
+        fi
 	else
 		echo "Para prosseguir com a nossa instalação, você precisa ter o Java instalado."
 		echo "Deseja instalar o Java? [Y/n]"
 		read res
 
-		if [ "$res" = "Y" ]
+		if [ "$res" = "Y" ] || [ "$res" = "y" ]
 			then
 			echo "Inicializando a instalação..."
-			# Atualizando os pacotes do sistema
-			sudo apt-get update && sudo apt-get upgrade -y
-			# Instalando o Java na versão 17
+			sudo apt-get update
 			sudo apt install openjdk-17-jdk -y
             nova_versao=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
 			echo "Java $nova_versao instalado com sucesso!"
 		else
 			echo  "O Java não será instalado."
 			echo "Finalizando execução..."
-			exit 1 # Finalizando o processo e printando status de erro para o usuário
+            sleep 2
+			exit 1
 		fi
 	fi
 
 	# Inicializando a instalação do .jar no repositório da Vita (Projeto de PI)
-	curl -o HWVitaCollector.jar -LJO https://github.com/vita-sptech/HWVitaCollector/tree/main/HWVitaCollector/out/artifacts/HWVitaCollector_jar/HWVitaCollector.jar
+	curl -LJO https://github.com/vita-sptech/HWVitaCollector/blob/main/HWVitaCollector/out/artifacts/HWVitaCollector_jar/HWVitaCollector.jar
+    
 
 	if [ $? -eq 0 ]
-		then
-		# Executando o .jar
-		java -jar HWVitaCollector.jar
+        then
+        if [ -f HWVitaCollector.jar ] # Verificando se o arquivo baixado é um arquivo .jar válido
+            then
+            echo "Iniciando o software..."
+		    java -jar HWVitaCollector.jar # Executando o arquivo .jar
+        else
+            echo "Erro ao rodar o .jar"
+            sleep 2
+		    exit 1
+        fi
 	else
-		echo "Erro ao rodar o .jar"
+		echo "Erro ao executar o curl"
+        sleep 2
 		exit 1
 	fi
 else
     echo "O software não será instalado."
     echo "Finalizando execução..."
-    exit 1 # Finalizando o processo e printando status de erro para o usuário
+    sleep 2
+    exit 1
 fi
