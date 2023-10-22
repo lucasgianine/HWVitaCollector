e1eae94bd2c393ad38dc12f4312a9cb05b93b632
Leopls <leonardobento233@gmail.com>
2023-10-21 21:17:15 -0300

    Melhorias na Main + Rebuild Artefato

--- HWVitaCollector/src/main/java/Main.java
+++ HWVitaCollector/src/main/java/Main.java
@@ -1,55 +1,46 @@
 import DAO.*;
-import com.github.britooo.looca.api.core.Looca;
-import com.github.britooo.looca.api.group.discos.DiscoGrupo;
-import com.github.britooo.looca.api.group.dispositivos.DispositivoUsb;
-import com.github.britooo.looca.api.group.memoria.Memoria;
-import com.github.britooo.looca.api.group.processador.Processador;
-import com.github.britooo.looca.api.group.sistema.Sistema;
-import com.github.britooo.looca.api.util.Conversor;
 import entidades.*;
 import gui.Login;
+import helpers.AsciiHelper;
 import helpers.HardwareExtractor;
 import helpers.VerificacaoHelper;
 import oshi.SystemInfo;
-import oshi.hardware.HWDiskStore;
-
-import java.awt.*;
-import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 @SuppressWarnings("SpellCheckingInspection")
 public class Main {
     public static void main(String[] args) {
         String UUID = new SystemInfo().getHardware().getComputerSystem().getHardwareUUID();
         boolean hasInterface = true;
-        try{
-        Login.mainLogin();
-        HardwareExtractor.HardwareExtractorLoop(UUID);
-        }catch (Exception e){
-            hasInterface = false;
-            System.out.println(e);
-        }
-
-        if(!hasInterface){
-            System.out.println(UUID);
-            //BLOCO PARA MÁQUINAS SEM INTERFACE --
-            Funcionario funcionario = FuncionarioDAO.getFuncionario("leo@gmail.com","senha@123");
-            if(VerificacaoHelper.maquinaIsCadastrada(UUID)){
-                System.out.println("Inicializando programa...");
-            }else{
-                String apelido = "Máquina Recepção, GUICHE 5";
-                Maquina maquina = new Maquina(UUID,funcionario.getFkHospital(),apelido,funcionario.getNome());
-                MaquinaDAO.registrarMaquina(maquina);
-                System.out.println("Cadastrando maquina...");
-                }
+        AsciiHelper.vitaHealthAscii();
+        try {
+            Login.mainLogin();
             HardwareExtractor.HardwareExtractorLoop(UUID);
+        }catch (Exception e) {
+            hasInterface = false;
+            System.out.println(e.getMessage());
+        }
+        if (!hasInterface) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Faça login com suas credenciais cadastradas");
+            System.out.println("Digite seu email: ");
+            String emailFuncionario = scanner.nextLine();
+            System.out.println("Digite sua senha: ");
+            String senhaFuncionario = scanner.nextLine();
+
+            Funcionario funcionario = FuncionarioDAO.getFuncionario(emailFuncionario, senhaFuncionario);
+            if (VerificacaoHelper.funcionarioIsAutenticado(funcionario)) {
+                if (!VerificacaoHelper.maquinaIsCadastrada(UUID)) {
+                    String apelido = "Máquina Recepção, GUICHE 5";
+                    Maquina maquina = new Maquina(UUID, funcionario.getFkHospital(), apelido, funcionario.getNome());
+                    MaquinaDAO.registrarMaquina(maquina);
+                    System.out.println("Cadastrando maquina...");
+                }
+            } else {
+                System.out.println("Registro não encontrado");
+                System.exit(404);
+            }
+                HardwareExtractor.HardwareExtractorLoop(UUID);
+            }
         }
     }
- }
-
-
-
-
