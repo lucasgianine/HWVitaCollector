632c509ee46eb0602ee691b42c9751abdf410ae1
VitorLpsDias <vitor.dias2000@sptech.school>
2023-11-24 00:10:26 -0300

    Console Login

--- HWVitaCollector/src/main/java/Main.java
+++ HWVitaCollector/src/main/java/Main.java
@@ -1,78 +1,62 @@
 import DAO.*;
-import com.mysql.cj.log.Log;
 import conexoes.Conexao;
 import conexoes.ConexaoNuvem;
 import entidades.*;
+import gui.ConsoleLogin;
 import gui.Login;
 import helpers.*;
 import integracaoSlack.Alertas;
 import oshi.SystemInfo;
-import java.util.*;
+
+import java.util.Scanner;
+
 @SuppressWarnings("SpellCheckingInspection")
 public class Main {
+
     public static void main(String[] args) {
         String UUID = new SystemInfo().getHardware().getComputerSystem().getHardwareUUID();
-        // BLOCO TRY PARA CRIAÇÃO DOS LOGS.
+
+        // Bloco try para criação dos logs.
         try {
             Logging logging = new Logging();
             logging.CreateLog();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.fillInStackTrace();
         }
 
-        //BLOCO TRY PARA FAZER AS CONEXÕES NECESSÁRIAS
-        try{
+        // Bloco try para fazer as conexões necessárias.
+        try {
             ConexaoNuvem.getConexaoNuvem();
             Conexao.getConexao();
-        }catch (Exception e){
+        } catch (Exception e) {
             String stackTrace = Helper.getStackTraceAsString(e);
-            Logging.AddLogInfo(Logging.fileHandler,e.getMessage() + stackTrace);
-            System.out.println(e.getMessage());
+            Logging.AddLogInfo(Logging.fileHandler, e.getMessage() + stackTrace);
+            System.out.println("Erro ao estabelecer conexões. Detalhes: " + e.getMessage());
         }
 
-
-        //IF PARA VERIFICAR SE CONEXÃO PRINCIPAL, A CONEXÃO QUE FAZ AS VALIDAÇÕES ESTA NULA, SE ESTIVER, GERAMOS UM LOG E ENCERRAMOS O PROGRAMA
-        if(Conexao.conn == null){
-            Logging.AddLogInfo(Logging.fileHandler,"Conexão com banco principal não foi estabelecida, programa interrompido: Main: 31");
+        // Verifica se a conexão principal, a conexão que faz as validações, está nula.
+        // Se estiver, gera um log e encerra o programa.
+        if (Conexao.conn == null) {
+            Logging.AddLogInfo(Logging.fileHandler, "Conexão com banco principal não foi estabelecida, programa interrompido: Main: 31");
             System.exit(0);
         }
 
-        //BLOCO DE LOGIN E INICIAÇÃO DO PROGRAMA VIA INTERFACE
+        // Bloco de login e iniciação do programa via interface.
         boolean hasInterface = true;
         AsciiHelper.vitaHealthAscii();
+
         try {
             Login.mainLogin();
             HardwareExtractor.HardwareExtractorLoop(UUID);
-        }catch (Exception e) {
+
+        } catch (Exception e) {
             hasInterface = false;
             String stackTrace = Helper.getStackTraceAsString(e);
-            Logging.AddLogInfo(Logging.fileHandler,stackTrace);
+            Logging.AddLogInfo(Logging.fileHandler, stackTrace);
         }
 
         if (!hasInterface) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Faça login com suas credenciais cadastradas");
-            System.out.print("Digite seu email: ");
-            String emailFuncionario = scanner.nextLine();
-            System.out.print("Digite sua senha: ");
-            String senhaFuncionario = scanner.nextLine();
-
-            Funcionario funcionario = FuncionarioDAO.getFuncionario(emailFuncionario, senhaFuncionario);
-            if (VerificacaoHelper.funcionarioIsAutenticado(funcionario)) {
-                if (!VerificacaoHelper.maquinaIsCadastrada(UUID)) {
-                    System.out.println("Cadastre sua máquina: ");
-                    System.out.print("Local: ");
-                    String local = scanner.nextLine();
-                    System.out.print("Cadastre o responsável: ");
-                    String responsavel = scanner.nextLine();
-                    Maquina maquina = new Maquina(UUID, funcionario.getFkHospital(), local, responsavel);
-                    MaquinaDAO.registrarMaquina(maquina);
-                }
-            } else {
-                System.out.println("Registro não encontrado");
-                System.exit(404);
-            }
-                HardwareExtractor.HardwareExtractorLoop(UUID);
-            }
+            ConsoleLogin.handleConsoleLogin(UUID);
         }
     }
+}
---
+++ HWVitaCollector/src/main/java/gui/ConsoleLogin.java
@@ -0,0 +1,49 @@
+package gui;
+import DAO.*;
+import entidades.*;
+import helpers.*;
+
+import java.util.Scanner;
+
+public class ConsoleLogin {
+
+    public static void handleConsoleLogin(String UUID) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Faça login com suas credenciais cadastradas");
+        System.out.print("Digite seu email: ");
+        String emailFuncionario = scanner.nextLine();
+        System.out.print("Digite sua senha: ");
+        String senhaFuncionario = scanner.nextLine();
+
+        Funcionario funcionario = FuncionarioDAO.getFuncionario(emailFuncionario, senhaFuncionario);
+
+        if (VerificacaoHelper.funcionarioIsAutenticado(funcionario)) {
+            if (!VerificacaoHelper.maquinaIsCadastrada(UUID)) {
+                System.out.println("╔════════════════════════════╗");
+                System.out.println(String.format("║ %1$-26s ║", "Cadastre sua máquina:"));
+                System.out.println("╠════════════════════════════╣");
+
+                System.out.print("║ Local: ");
+                String local = scanner.nextLine();
+                System.out.print("║ Cadastre o responsável: ");
+                String responsavel = scanner.nextLine();
+
+                Maquina maquina = new Maquina(UUID, funcionario.getFkHospital(), local, responsavel);
+                MaquinaDAO.registrarMaquina(maquina);
+
+                System.out.println("╚════════════════════════════╝");
+                System.out.println("Máquina cadastrada com sucesso!");
+            }
+        } else {
+            System.out.println("╔════════════════════════════╗");
+            System.out.println(String.format("║ %1$-26s ║", "Credenciais inválidas."));
+            System.out.println("╠════════════════════════════╣");
+            System.out.println("║ Programa encerrado.        ║");
+            System.out.println("╚════════════════════════════╝");
+            System.exit(404);
+        }
+
+        HardwareExtractor.HardwareExtractorLoop(UUID);
+    }
+}
